ba33399291027f13fc0e0290b0ae0948e7e2770a
pateldeep93 <deep@eternalsoftsolutions.com>
2017-09-20 21:40:08 +0530

    Json learn

---
+++ demo.php
@@ -0,0 +1,24 @@
+<html>
+	<body>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script>
+			var thepets = [
+				{
+					"name": "Sumit",
+					"from": "ahm"	
+				},
+				{
+					"name": "vihar",
+					"from": "rajkot"
+				}
+			]
+
+			var store = thepets[1].name;
+			alert(store);
+		</script>
+
+	</body>
+	<head>
+		<div id="container">sumit</div>
+	</head>
+</html>
